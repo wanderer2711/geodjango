94c31a7441e71865930df4e396c5be730249265a
suhridh <suhridhsundaram@gmail.com>
2021-07-07 09:53:54 +0530

    added pop ups

--- weather/views.py
+++ weather/views.py
@@ -12,12 +12,13 @@ from django.core.serializers import serialize
 
 def index(request):
     l = Location.objects.filter(point__isnull=False)
+    weather = []
     for i in l:
         lat = str(i.point.coords[1])
         lon = str(i.point.coords[0])
         unit = "metric"
 
-        model = Location
+        # model = Location
         api_key = "7aad66ca433ee2d64650f4c17ce798e8"
         url = 'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=7aad66ca433ee2d64650f4c17ce798e8'
         base_url = 'http://api.openweathermap.org/data/2.5/weather?'
@@ -26,15 +27,19 @@ def index(request):
         comp_url = base_url +   "lat=" + lat + "&lon=" + lon + "&appid=" + api_key + "&units=" + unit
         
         q = requests.get(url=comp_url).json()
-        print(q)
+        # print(q)
 
         city_weather = {
-            'city': ['name'], 
+            'city': q['name'], 
             'temperature': q['main']['temp'],
-            'humidity': q['main']['humidity']
+            'humidity': q['main']['humidity'],
+            'lat' : q['coord']['lat'],
+            'lon': q['coord']['lon']
         }
+        weather.append(city_weather)
+    print(weather)
 
-        data = {'l' : l, 'city_weather': city_weather, 'lat': lat, 'lon': lon}
+    data = {'l' : l, 'weather': weather} #'lat': lat, 'lon': lon}
 
     return render(request, 'weather/weather.html', data)
 
